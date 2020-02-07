c37d51bcdb8ff926485dbe2401409c44b4f51025
Oscar <oscarjavierho@gmail.com>
2020-02-07 08:18:01 -0430

    Minor change

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="footer"> 
-      <footer className="text-center p-1 bg-light">
+    <div className="footer bg-light"> 
+      <footer className="text-center p-1">
         Copyright &copy; {new Date().getFullYear()} Oscar Hernandez 
       </footer>
     </div>
