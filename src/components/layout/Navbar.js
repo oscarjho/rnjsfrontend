9501057bc88c2fcf86443e3ad081cef3c91d2e23
Oscar <oscarjavierho@gmail.com>
2020-02-09 08:42:18 -0430

    Navbar fix

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
   render() {
+
+    const guestLinks = (
+
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a href="/register"> 
+            <button type="button" className="btn btn-info">Sign Up</button> 
+          </a> 
+        </li>
+        <li className="nav-item">
+        &nbsp; 
+        </li>
+        <li className="nav-item">
+          <a href="/login"> 
+          <button type="button" className="btn btn-info">Sign In</button> 
+          </a>
+        </li>  
+        </ul>
+    );
+    
     return (
-        <div className="navbar bg-light">
+        <div className="navbar navbar-expand-sm navbar-light bg-light">
           <div className="container">
-            <div>
-              <a href="/"> 
-                <h5 className="text-dark"> Mern Example</h5>
+
+              <a className="navbar-brand" href="/"> 
+                <h5 className="text-dark col-8"> Mern Example</h5>
               </a>
-            </div>
-            <div> 
-              <a href="/register"> 
-                <button type="button" className="btn btn-info" data-toggle="modal" data-target="#exampleModal"> Sign In </button>
-              </a>
-            </div>
+
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#mobile-nav"
+              >
+              <span className="navbar-toggler-icon"></span>
+              </button>
+
+              <div className="collapse navbar-collapse" id="mobile-nav">
+                {guestLinks}
+              </div>
 
           </div>
         </div>
