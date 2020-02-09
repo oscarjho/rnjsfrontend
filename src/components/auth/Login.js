abdedf6a7f0fbe2c8a3f90917373d9a24383cff0
Oscar <oscarjavierho@gmail.com>
2020-02-09 14:26:29 -0430

    Minor fix

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -39,7 +39,7 @@ export class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">
-                Sign in to your DevConnector account
+                Sign in to your account
               </p>
               <form onSubmit={this.onSubmit}>
                 <div>
